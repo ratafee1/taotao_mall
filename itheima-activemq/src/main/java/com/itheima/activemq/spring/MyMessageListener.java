c06e44c19693d8fa366ee0c2d7fb78e5a7388559
ratafee <github@com>
2019-12-08 00:48:27 +0800

    first commit

---
+++ itheima-activemq/src/main/java/com/itheima/activemq/spring/MyMessageListener.java
@@ -0,0 +1,26 @@
+package com.itheima.activemq.spring;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.xml.soap.Text;
+
+public class MyMessageListener implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		//获取消息
+		if(message instanceof TextMessage){
+			TextMessage textMessage = (TextMessage)message;
+			String text;
+			try {
+				text = textMessage.getText();
+				System.out.println(text);
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
